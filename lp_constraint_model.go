e4a407ab37fb3bb323c1b0dad2c08a3507e2df84
willauld <will.auld@gmail.com>
2018-01-18 18:49:57 -0800

    updating to current version (still not ready to run).

--- lp_constraint_model.go
+++ lp_constraint_model.go
@@ -1,10 +1,12 @@
 package rplanlib
 
+import "strconv"
 
 type modelSpecs struct {
     InputParams map[string]string
-    vindx vectorVarIndex
+    vindx VectorVarIndex
 
+    ti Taxinfo
     // The following was through 'S'
     numyr int
     maximize string // "Spending" or "PlusEstate"
@@ -30,14 +32,72 @@ type modelSpecs struct {
 
 } 
 
-func NewModelSpecs(vindx vectorVarIndex/*, 
-                    taxtable, capgainstable, penalty, stded, SS_taxable, verbose, no_TDRA_ROTHRA_DEPOSITS*/
-                                        ) modelSpecs {
-    return modelSpecs{
-    InputParams: map[string]string
-    vindx: vindx,
-    numyr: int
-    maximize: string // "Spending" or "PlusEstate"
+func intmax(a, b int) int {
+    if a>b {
+        return a
+    } else {
+        return b
+    }
+}
+func intmin(a, b int) int {
+    if a<b {
+        return a
+    } else {
+        return b
+    }
+}
+func checkStrconvError(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func NewModelSpecs(vindx VectorVarIndex, 
+                   ti Taxinfo, 
+                   ip map[string]string,
+                   verbose bool, 
+                   no_TDRA_ROTHRA_DEPOSITS bool) modelSpecs {
+    ms := modelSpecs{
+        InputParams: ip,
+        vindx: vindx,
+        ti: ti,
+        maximize: "Spending", // or "PlusEstate"
+        iRate: 1.025,
+        rRate: 1.06,
+        min: -1,
+        max: -1,
+    }
+    /*
+    ** TODO what if any of the strings returned by ip[] are empty? FIXME
+    */
+    if xxxx == "joint" {
+    age1, err := strconv.Atoi(ip["eT_Age1"])
+    checkStrconvError(err) 
+    age2, err := strconv.Atoi(ip["eT_Age2"])
+    checkStrconvError(err) 
+    retireAge1, err := strconv.Atoi(ip["eT_RetireAge1"])
+    checkStrconvError(err) 
+    if retireAge1 < age1 {
+        retireAge1 = age1
+    }
+    retireAge2, err := strconv.Atoi(ip["eT_RetireAge2"])
+    checkStrconvError(err) 
+    if retireAge2 < age2 {
+        retireAge2 = age2
+    }
+    planThroughAge1, err := strconv.Atoi(ip["eT_PlanThroughAge1"])
+    checkStrconvError(err) 
+    planThroughAge2, err := strconv.Atoi(ip["eT_PlanThroughAge2"])
+    checkStrconvError(err) 
+    yearsToRetire1 := retireAge1 - age1
+    yearsToRetire2 := retireAge2 - age2
+    yearsToRetire := intmin(yearsToRetire1, yearsToRetire2)
+    startPlan := yearsToRetire + age1
+    through1 := planThroughAge1 - age1
+    through2 := planThroughAge2 - age2
+    endPlan := intmax(through1, through2) + 1 + age1
+    delta := age1 - age2
+    ms.numyr := endPlan - startPlan
     accounttable: []map[string]string
     accmap: map[string]int
     retiree: []map[string]string
@@ -47,22 +107,15 @@ func NewModelSpecs(vindx vectorVarIndex/*,
     taxed: []float64
     asset_sale: []float64
     cg_asset_taxed: []float64
-    iRate: float64
-    rRate: float64
-    min: float64
-    max: float64
+    } else { // single or mseparate
+
     }
+    return ms
 }
 /*
 class lp_constraint_model:
-    def __init__(self, S, vindx, taxtable, capgainstable, penalty, stded, SS_taxable, verbose, no_TDRA_ROTHRA_DEPOSITS):
+    def __init__(verbose, no_TDRA_ROTHRA_DEPOSITS):
         self.S = S
-        self.var_index = vindx
-        self.taxtable = taxtable
-        self.cgtaxtable = capgainstable
-        self.penalty = penalty
-        self.stded = stded
-        self.ss_taxable = SS_taxable
         self.verbose = verbose
         self.noTdraRothraDeposits = no_TDRA_ROTHRA_DEPOSITS
 
@@ -76,11 +129,6 @@ func (ms modelSpecs) BuildModel() ([]float64, [][]float64, []float64) {
     
         // TODO integrate the following assignments into the code and remove them
         //S = ms.S
-        taxtable = ms.taxtable
-        capgainstable = ms.cgtaxtable
-        penalty = ms.penalty
-        stded = ms.stded
-        SS_taxable = ms.ss_taxable
     
         nvars = ms.vindx.Vsize
         A = make([][]float64, 0)
@@ -97,7 +145,7 @@ func (ms modelSpecs) BuildModel() ([]float64, [][]float64, []float64) {
         // Add objective function tax bracket forcing function (EXPERIMENTAL)
         //
         for year = 0; year<ms.numyr; year++ {
-            for k := 0; k<len(taxtable); k++ {
+            for k := 0; k<len(ms.ti.taxtable); k++ {
                 // multiplies the impact of higher brackets opposite to
                 // optimization the intent here is to pressure higher 
                 // brackets more and pack the lower brackets
@@ -132,17 +180,17 @@ func (ms modelSpecs) BuildModel() ([]float64, [][]float64, []float64) {
                 p = 1
                 if ms.accounttable[j]["acctype"] != "aftertax"{
                     if S.apply_early_penalty(year,ms.accounttable[j]["mykey"]){
-                        p = 1-penalty
+                        p = 1-ms.ti.penalty
                     }
                 }
                 row[ms.vindx.W(year,j)] = -1*p 
             }
-            for k:=0;  k<len(taxtable);k++ {
-                row[ms.vindx.X(year,k)] = taxtable[k][2] // income tax
+            for k:=0;  k<len(ms.ti.taxtable);k++ {
+                row[ms.vindx.X(year,k)] = ms.ti.taxtable[k][2] // income tax
             } 
             if ms.accmap["aftertax"] > 0 {
-                for l:=0; l<len(capgainstable); l++ {
-                    row[ms.vindx.Y(year,l)] = capgainstable[l][2] // cap gains tax
+                for l:=0; l<len(ms.ti.capgainstable); l++ {
+                    row[ms.vindx.Y(year,l)] = ms.ti.capgainstable[l][2] // cap gains tax
                 } 
                 for j := 0; j<len(ms.accounttable); j++ {
                     row[ms.vindx.D(year,j)] = 1
@@ -319,21 +367,21 @@ func (ms modelSpecs) BuildModel() ([]float64, [][]float64, []float64) {
                     row[ms.vindx.D(year,j)] = -1 // Account 0 is TDRA
                 }
             } 
-            for k:=0;  k<len(taxtable);k++ {
+            for k:=0;  k<len(ms.ti.taxtable);k++ {
                 row[ms.vindx.X(year,k)] = -1
             }
             A = append(A, row)
-            b = append(b, stded*adj_inf-ms.taxed[year]-SS_taxable*ms.SS[year])
+            b = append(b, ms.ti.stded*adj_inf-ms.taxed[year]-ms.ti.SStaxable*ms.SS[year])
         }
         //
         // Add constraints for (12')
         //
         for year = 0; year<ms.numyr; year++ {
-            for k:=0;  k<len(taxtable)-1;k++ {
+            for k:=0;  k<len(ms.ti.taxtable)-1;k++ {
                 row = [0] * nvars
                 row[ms.vindx.X(year,k)] = 1
                 A = append(A, row)
-                b = append(b, (taxtable[k][1])*(ms.i_rate**(ms.preplanyears+year))) // inflation adjusted
+                b = append(b, (ms.ti.taxtable[k][1])*(ms.i_rate**(ms.preplanyears+year))) // inflation adjusted
             }
         }
         //
@@ -343,7 +391,7 @@ func (ms modelSpecs) BuildModel() ([]float64, [][]float64, []float64) {
             for year:=0; year<ms.numyr; year++ {
                 f = ms.cg_taxable_fraction(year)
                 row = [0] * nvars
-                for l:=0; l<len(capgainstable);l++{
+                for l:=0; l<len(ms.ti.capgainstable);l++{
                     row[ms.vindx.Y(year,l)] = 1
                 }
                 // Awful Hack! If year of asset sale, assume w(i,j)-D(i,j) is 
@@ -371,7 +419,7 @@ func (ms modelSpecs) BuildModel() ([]float64, [][]float64, []float64) {
                     row[ms.vindx.W(year,j)] = f 
                     row[ms.vindx.D(year,j)] = -f 
                 }
-                for l:=0; l<len(capgainstable);l++{
+                for l:=0; l<len(ms.ti.capgainstable);l++{
                     row[ms.vindx.Y(year,l)] = -1
                 }
                 A = append(A, row)
@@ -384,17 +432,17 @@ func (ms modelSpecs) BuildModel() ([]float64, [][]float64, []float64) {
         if ms.accmap["aftertax"] > 0{
             for year:=0; year<ms.numyr; year++ {
                 adj_inf = ms.i_rate**(ms.preplanyears+year)
-                for l:=0; l<len(capgainstable)-1;l++{
+                for l:=0; l<len(ms.ti.capgainstable)-1;l++{
                     row = [0] * nvars
                     row[ms.vindx.Y(year,l)] = 1
-                    for k:=0;  k<len(taxtable)-1;k++ {
-                        if taxtable[k][0] >= capgainstable[l][0] && taxtable[k][0] < capgainstable[l+1][0] {
+                    for k:=0;  k<len(ms.ti.taxtable)-1;k++ {
+                        if ms.ti.taxtable[k][0] >= ms.ti.capgainstable[l][0] && ms.ti.taxtable[k][0] < ms.ti.capgainstable[l+1][0] {
                             row[ms.vindx.X(year,k)] = 1
                         }
                     }
                     A = append(A, row)
-                    b = append(b, capgainstable[l][1]*adj_inf) // mcg[i,l] inflation adjusted
-                    //print_constraint( row, capgainstable[l][1]*adj_inf)
+                    b = append(b, ms.ti.capgainstable[l][1]*adj_inf) // mcg[i,l] inflation adjusted
+                    //print_constraint( row, ms.ti.capgainstable[l][1]*adj_inf)
                 }
             }
         }
@@ -526,14 +574,9 @@ func (ms modelSpecs) print_constraint(row []float64, b float64) {
     
 func (ms modelSpecs) print_model_row(row []float64, suppress_newline bool) {
         S = ms.S
-        taxtable = ms.taxtable
-        capgainstable = ms.cgtaxtable
-        penalty = ms.penalty
-        stded = ms.stded
-        SS_taxable = ms.ss_taxable
     
         for i:=0; i<ms.numyr; i++ {
-            for k:=0;  k<len(taxtable);k++ {
+            for k:=0;  k<len(ms.ti.taxtable);k++ {
                 if row[ms.vindx.X(i, k)] != 0{
                     fmt.Printf("x[%d,%d]: %6.3f", i, k, row[ms.vindx.X(i, k)])
                 }
@@ -541,7 +584,7 @@ func (ms modelSpecs) print_model_row(row []float64, suppress_newline bool) {
         }
         if ms.accmap["aftertax"] > 0 {
             for i:=0; i<ms.numyr; i++ {
-                for l:=0; l<len(capgainstable);l++{
+                for l:=0; l<len(ms.ti.capgainstable);l++{
                     if row[ms.vindx.Y(i, l)] != 0{
                         fmt.Printf("y[%d,%d]: %6.3f ", i, l, row[ms.vindx.Y(i, l)])
                     }

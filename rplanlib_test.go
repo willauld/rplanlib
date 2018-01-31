3a8effafb45feaf1ef10f9186715cf9c03b8eed8
willauld <will.auld@gmail.com>
2018-01-31 12:58:44 -0800

    Adding unit test for printConstraint().

--- rplanlib_test.go
+++ rplanlib_test.go
@@ -1374,14 +1374,164 @@ func TestPrintModelMatrix(t *testing.T) { /* TODO:FIXME:IMPLEMENTME */
 }
 
 func TestPrintConstraint(t *testing.T) { /* TODO:FIXME:IMPLEMENTME */
-	/*
-		tests := []struct {
-		}{
-			{},
+	tests := []struct {
+		ip        map[string]string
+		row       []float64
+		b         float64
+		expectstr string
+		testcase  string
+	}{
+		{ // Case 0
+			ip: map[string]string{
+				"setName":                    "activeParams",
+				"filingStatus":               "single",
+				"eT_Age1":                    "60",
+				"eT_Age2":                    "",
+				"eT_RetireAge1":              "65",
+				"eT_RetireAge2":              "",
+				"eT_PlanThroughAge1":         "70",
+				"eT_PlanThroughAge2":         "",
+				"eT_PIA1":                    "20", // 20k
+				"eT_PIA2":                    "",
+				"eT_SS_Start1":               "70",
+				"eT_SS_Start2":               "",
+				"eT_TDRA1":                   "10", // 10k
+				"eT_TDRA2":                   "",
+				"eT_TDRA_Rate1":              "",
+				"eT_TDRA_Rate2":              "",
+				"eT_TDRA_Contrib1":           "",
+				"eT_TDRA_Contrib2":           "",
+				"eT_TDRA_ContribStartAge1":   "",
+				"eT_TDRA_ContribStartAge2":   "",
+				"eT_TDRA_ContribEndAge1":     "",
+				"eT_TDRA_ContribEndAge2":     "",
+				"eT_Roth1":                   "5", // 5k
+				"eT_Roth2":                   "",
+				"eT_Roth_Rate1":              "",
+				"eT_Roth_Rate2":              "",
+				"eT_Roth_Contrib1":           "",
+				"eT_Roth_Contrib2":           "",
+				"eT_Roth_ContribStartAge1":   "",
+				"eT_Roth_ContribStartAge2":   "",
+				"eT_Roth_ContribEndAge1":     "",
+				"eT_Roth_ContribEndAge2":     "",
+				"eT_Aftatax":                 "15", // 15k
+				"eT_Aftatax_Rate":            "",
+				"eT_Aftatax_Contrib":         "",
+				"eT_Aftatax_ContribStartAge": "",
+				"eT_Aftatax_ContribEndAge":   "",
+			},
+			row:       []float64{},
+			b:         743.027,
+			expectstr: "Row: [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] b: 743.027 <= b[]: 743.03 ",
+			testcase:  "allzeros",
+		},
+		{ // Case 1
+			ip: map[string]string{
+				"setName":                    "activeParams",
+				"filingStatus":               "single",
+				"eT_Age1":                    "60",
+				"eT_Age2":                    "",
+				"eT_RetireAge1":              "65",
+				"eT_RetireAge2":              "",
+				"eT_PlanThroughAge1":         "70",
+				"eT_PlanThroughAge2":         "",
+				"eT_PIA1":                    "20", // 20k
+				"eT_PIA2":                    "",
+				"eT_SS_Start1":               "70",
+				"eT_SS_Start2":               "",
+				"eT_TDRA1":                   "10", // 10k
+				"eT_TDRA2":                   "",
+				"eT_TDRA_Rate1":              "",
+				"eT_TDRA_Rate2":              "",
+				"eT_TDRA_Contrib1":           "",
+				"eT_TDRA_Contrib2":           "",
+				"eT_TDRA_ContribStartAge1":   "",
+				"eT_TDRA_ContribStartAge2":   "",
+				"eT_TDRA_ContribEndAge1":     "",
+				"eT_TDRA_ContribEndAge2":     "",
+				"eT_Roth1":                   "5", // 5k
+				"eT_Roth2":                   "",
+				"eT_Roth_Rate1":              "",
+				"eT_Roth_Rate2":              "",
+				"eT_Roth_Contrib1":           "",
+				"eT_Roth_Contrib2":           "",
+				"eT_Roth_ContribStartAge1":   "",
+				"eT_Roth_ContribStartAge2":   "",
+				"eT_Roth_ContribEndAge1":     "",
+				"eT_Roth_ContribEndAge2":     "",
+				"eT_Aftatax":                 "15", // 15k
+				"eT_Aftatax_Rate":            "",
+				"eT_Aftatax_Contrib":         "",
+				"eT_Aftatax_ContribStartAge": "",
+				"eT_Aftatax_ContribEndAge":   "",
+			},
+			row:       []float64{},
+			b:         743.027,
+			expectstr: "Row: [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127 128 129 130 131 132 133 134 135 136 137 138 139 140 141 142 143 144 145 146 147 148 149 150 151 152 153 154 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175 176 177 178 179 180 181 182 183 184 185 186 187 188 189 190 191 192 193 194 195 196 197 198 199 200 201 202 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 220 221 222] b: 743.027 x[0,1]= 1.000, x[0,2]= 2.000, x[0,3]= 3.000, x[0,4]= 4.000, x[0,5]= 5.000, x[0,6]= 6.000, x[1,0]= 7.000, x[1,1]= 8.000, x[1,2]= 9.000, x[1,3]=10.000, x[1,4]=11.000, x[1,5]=12.000, x[1,6]=13.000, x[2,0]=14.000, x[2,1]=15.000, x[2,2]=16.000, x[2,3]=17.000, x[2,4]=18.000, x[2,5]=19.000, x[2,6]=20.000, x[3,0]=21.000, x[3,1]=22.000, x[3,2]=23.000, x[3,3]=24.000, x[3,4]=25.000, x[3,5]=26.000, x[3,6]=27.000, x[4,0]=28.000, x[4,1]=29.000, x[4,2]=30.000, x[4,3]=31.000, x[4,4]=32.000, x[4,5]=33.000, x[4,6]=34.000, x[5,0]=35.000, x[5,1]=36.000, x[5,2]=37.000, x[5,3]=38.000, x[5,4]=39.000, x[5,5]=40.000, x[5,6]=41.000, x[6,0]=42.000, x[6,1]=43.000, x[6,2]=44.000, x[6,3]=45.000, x[6,4]=46.000, x[6,5]=47.000, x[6,6]=48.000, x[7,0]=49.000, x[7,1]=50.000, x[7,2]=51.000, x[7,3]=52.000, x[7,4]=53.000, x[7,5]=54.000, x[7,6]=55.000, x[8,0]=56.000, x[8,1]=57.000, x[8,2]=58.000, x[8,3]=59.000, x[8,4]=60.000, x[8,5]=61.000, x[8,6]=62.000, x[9,0]=63.000, x[9,1]=64.000, x[9,2]=65.000, x[9,3]=66.000, x[9,4]=67.000, x[9,5]=68.000, x[9,6]=69.000, x[10,0]=70.000, x[10,1]=71.000, x[10,2]=72.000, x[10,3]=73.000, x[10,4]=74.000, x[10,5]=75.000, x[10,6]=76.000, y[0,0]=77.000, y[0,1]=78.000, y[0,2]=79.000, y[1,0]=80.000, y[1,1]=81.000, y[1,2]=82.000, y[2,0]=83.000, y[2,1]=84.000, y[2,2]=85.000, y[3,0]=86.000, y[3,1]=87.000, y[3,2]=88.000, y[4,0]=89.000, y[4,1]=90.000, y[4,2]=91.000, y[5,0]=92.000, y[5,1]=93.000, y[5,2]=94.000, y[6,0]=95.000, y[6,1]=96.000, y[6,2]=97.000, y[7,0]=98.000, y[7,1]=99.000, y[7,2]=100.000, y[8,0]=101.000, y[8,1]=102.000, y[8,2]=103.000, y[9,0]=104.000, y[9,1]=105.000, y[9,2]=106.000, y[10,0]=107.000, y[10,1]=108.000, y[10,2]=109.000, w[0,0]=110.000, w[0,1]=111.000, w[0,2]=112.000, w[1,0]=113.000, w[1,1]=114.000, w[1,2]=115.000, w[2,0]=116.000, w[2,1]=117.000, w[2,2]=118.000, w[3,0]=119.000, w[3,1]=120.000, w[3,2]=121.000, w[4,0]=122.000, w[4,1]=123.000, w[4,2]=124.000, w[5,0]=125.000, w[5,1]=126.000, w[5,2]=127.000, w[6,0]=128.000, w[6,1]=129.000, w[6,2]=130.000, w[7,0]=131.000, w[7,1]=132.000, w[7,2]=133.000, w[8,0]=134.000, w[8,1]=135.000, w[8,2]=136.000, w[9,0]=137.000, w[9,1]=138.000, w[9,2]=139.000, w[10,0]=140.000, w[10,1]=141.000, w[10,2]=142.000, b[0,0]=143.000, b[0,1]=144.000, b[0,2]=145.000, b[1,0]=146.000, b[1,1]=147.000, b[1,2]=148.000, b[2,0]=149.000, b[2,1]=150.000, b[2,2]=151.000, b[3,0]=152.000, b[3,1]=153.000, b[3,2]=154.000, b[4,0]=155.000, b[4,1]=156.000, b[4,2]=157.000, b[5,0]=158.000, b[5,1]=159.000, b[5,2]=160.000, b[6,0]=161.000, b[6,1]=162.000, b[6,2]=163.000, b[7,0]=164.000, b[7,1]=165.000, b[7,2]=166.000, b[8,0]=167.000, b[8,1]=168.000, b[8,2]=169.000, b[9,0]=170.000, b[9,1]=171.000, b[9,2]=172.000, b[10,0]=173.000, b[10,1]=174.000, b[10,2]=175.000, b[11,0]=176.000, b[11,1]=177.000, b[11,2]=178.000, s[0]=179.000, s[1]=180.000, s[2]=181.000, s[3]=182.000, s[4]=183.000, s[5]=184.000, s[6]=185.000, s[7]=186.000, s[8]=187.000, s[9]=188.000, s[10]=189.000, D[0,0]=190.000, D[0,1]=191.000, D[0,2]=192.000, D[1,0]=193.000, D[1,1]=194.000, D[1,2]=195.000, D[2,0]=196.000, D[2,1]=197.000, D[2,2]=198.000, D[3,0]=199.000, D[3,1]=200.000, D[3,2]=201.000, D[4,0]=202.000, D[4,1]=203.000, D[4,2]=204.000, D[5,0]=205.000, D[5,1]=206.000, D[5,2]=207.000, D[6,0]=208.000, D[6,1]=209.000, D[6,2]=210.000, D[7,0]=211.000, D[7,1]=212.000, D[7,2]=213.000, D[8,0]=214.000, D[8,1]=215.000, D[8,2]=216.000, D[9,0]=217.000, D[9,1]=218.000, D[9,2]=219.000, D[10,0]=220.000, D[10,1]=221.000, D[10,2]=222.000, <= b[]: 743.03 ",
+			testcase:  "counting",
+		},
+	}
+	for i, elem := range tests {
+		ip := NewInputParams(elem.ip)
+		ti := NewTaxInfo(ip.filingStatus)
+		taxbins := len(*ti.Taxtable)
+		cgbins := len(*ti.Capgainstable)
+		//fmt.Printf("ACCMAP: %v", ip.accmap)
+		vindx, err := NewVectorVarIndex(ip.numyr, taxbins, cgbins, ip.accmap)
+		if err != nil {
+			t.Errorf("PrintConstraint case %d: %s", i, err)
+			continue
 		}
-		for i, elem := range tests {
+		numaccounts := 0
+		for _, acc := range ip.accmap {
+			numaccounts += acc
 		}
-	*/
+		ms := ModelSpecs{
+			ip:     ip,
+			vindx:  vindx,
+			ti:     ti,
+			numyr:  ip.numyr,
+			accmap: ip.accmap,
+			numacc: numaccounts,
+		}
+
+		row := make([]float64, vindx.Vsize)
+		switch elem.testcase {
+		case "allones":
+			for indx := 0; indx < vindx.Vsize; indx++ {
+				row[indx] = float64(indx)
+			}
+		case "counting":
+			for indx := 0; indx < vindx.Vsize; indx++ {
+				row[indx] = float64(indx)
+			}
+		case "allzeros":
+			// nothing to change
+		default:
+			t.Errorf("TestPrintModelRow: Unexpected test case '%s'\n", elem.testcase)
+			continue
+		}
+		//fmt.Printf("Vsize: %d\n", vindx.Vsize)
+
+		mychan := make(chan string)
+		oldout, w, err := RedirectStdout(mychan)
+		if err != nil {
+			t.Errorf("RedirectStdout: %s\n", err)
+			return // should this be continue?
+		}
+		fmt.Printf("Row: %v\n", row)
+		fmt.Printf("b: %v\n", elem.b)
+		//ms.printModelRow(row, elem.suppressNewline)
+		ms.printConstraint(row, elem.b)
+
+		str := RestoreStdout(mychan, oldout, w)
+		strn := stripWhitespace(str)
+		strexpect := stripWhitespace(elem.expectstr)
+		if strn != strexpect {
+			t.Errorf("PrintModelRow Case %d: expected '%s', found '%s'", i, elem.expectstr, str)
+		}
+	}
 }
 
 func TestPrintModelRow(t *testing.T) {

1655aa1ee7d06f19806a9d80e24b5dfda642e1a0
willauld <will.auld@gmail.com>
2018-02-02 15:32:32 -0800

    Adding unit tests for socialsecurity.go, current test is test fra.

--- socialsecurity.go
+++ socialsecurity.go
@@ -2,122 +2,165 @@ package rplanlib
 
 import "math"
 import "fmt"
-import "os"
+import "time"
 
-func startamount(amount float64, fra int, start int) float64 {
-    if start > 70 {
-        start = 70
+func fra(currAge int) int {
+	thisYear := time.Now().Year()
+	born := thisYear - currAge
+	if born < 1940 {
+		return 65
+	} else if born < 1957 {
+		return 66
 	}
-    if start < 62 {
-        start = 62
-	}
-    if start < fra {
-        return amount/(math.Pow(1.067,float64(fra-start)))
-	}
-	//  start >= fra must be
-    return amount*(math.Pow(1.08,float64(start-fra)))
+	return 67
 }
 
-func do_SS_details(S map[string]string , bucket []float64) {
-    sections := 0
-    index := 0
-    itype := "SocialSecurity"
-    for k,v := range S.itype { //S.get( itype , {}).items()) 
-                sections += 1
-                r := match_retiree(k)
-                if r == nil {
-                    fmt.Printf("Error: [%s.%s] must match a retiree\n\t[%s.%s] should match [iam.%s] but there is no [iam.%s]\n", itype,k,itype,k,k,k)
-                    os.Exit(1)
-				}
-                fraamount := v["amount"]
-                fraage := v["FRA"]
-                agestr := v["age"]
-                dt := map[string]float64{"key": k, "amount": fraamount, "fra": fraage, "agestr": agestr, "ageAtStart": r["ageAtStart"], "currAge": r["age"], "throughAge": r["through"]}
-                if fraamount < 0 && sections == 1 { // default spousal support in second slot 
-                    self.SSinput[1] = dt
-				} else {
-                    self.SSinput[index] = dt
-                    index += 1
-				}
+func startamount(amount float64, fra int, start int) float64 {
+	if start > 70 {
+		start = 70
 	}
-            for i := range sections {
-                //print("SSinput", self.SSinput)
-                agestr = self.SSinput[i]["agestr"]
-                firstage = agelist(agestr)
-                disperseage = next(firstage)
-                if i == 0 {
-                    firstdisperseage = disperseage
-                    firstdisperseyear = disperseage - self.SSinput[0]["ageAtStart"]
-				}
-                fraage = self.SSinput[i]["fra"]
-                fraamount = self.SSinput[i]["amount"]
-                ageAtStart = self.SSinput[i]["ageAtStart"]
-                currAge = self.SSinput[i]["currAge"]
-                if fraamount >= 0 {
-                    // alter amount for start age vs fra (minus if before fra and + is after)
-                    amount = self.startamount(fraamount, fraage, disperseage)
+	if start < 62 {
+		start = 62
+	}
+	if start < fra {
+		return amount / (math.Pow(1.067, float64(fra-start)))
+	}
+	//  start >= fra must be
+	return amount * (math.Pow(1.08, float64(start-fra)))
+}
+
+type SSInput struct {
+	fraamount  int
+	fraage     int
+	startAge   int
+	endAge     int
+	key        string
+	ageAtStart int
+	currAge    int
+	bucket     []float64
+}
+
+func processSS(ip InputParams, r []retiree, iRate float64) (SS, SS1, SS2 []float64, e error) {
+
+	SSinput := make([]SSInput, 2)
+	if ip.PIA1 < 0 && ip.PIA2 < 0 {
+		e := fmt.Errorf("processSS: both PIA1: %d and PIA2: %d are negitive", ip.PIA1, ip.PIA2)
+		return nil, nil, nil, e
+	}
+	SS = make([]float64, ip.numyr)
+
+	sections := 0
+	index := 0
+	sections++
+	dt := SSInput{
+		fraamount:  ip.PIA1,            // fraamount := v["amount"]
+		fraage:     fra(ip.age1),       // fraage := v["FRA"]
+		startAge:   ip.SSStart1,        // agestr := v["age"]
+		endAge:     ip.planThroughAge1, // agestr := v["age"]
+		key:        ip.myKey1,
+		ageAtStart: ip.SSStart1,
+		currAge:    ip.age1,
+	}
+	if dt.fraamount < 0 && sections == 1 { // default spousal support in second slot
+		SSinput[1] = dt
+	} else {
+		SSinput[index] = dt
+		index++
+	}
+	sections++
+	dt = SSInput{
+		fraamount:  ip.PIA2,            // fraamount := v["amount"]
+		fraage:     fra(ip.age2),       // fraage := v["FRA"]
+		startAge:   ip.SSStart2,        // agestr := v["age"]
+		endAge:     ip.planThroughAge2, // agestr := v["age"]
+		key:        ip.myKey2,
+		ageAtStart: ip.SSStart2,
+		currAge:    ip.age2,
+	}
+	if dt.fraamount < 0 && sections == 1 { // default spousal support in second slot
+		SSinput[1] = dt
+	} else {
+		SSinput[index] = dt
+		index++
+	}
+	var disperseage int
+	var firstdisperseyear int
+	for i := 0; i < sections; i++ {
+		disperseage = SSinput[i].startAge
+		if i == 0 {
+			firstdisperseyear = disperseage - SSinput[0].ageAtStart
+		}
+		fraage := SSinput[i].fraage
+		fraamount := SSinput[i].fraamount
+		ageAtStart := SSinput[i].ageAtStart
+		currAge := SSinput[i].currAge
+		var amount float64
+		if fraamount >= 0 {
+			// alter amount for start age vs fra (minus if before fra and + is after)
+			amount = startamount(float64(fraamount), fraage, disperseage)
+		} else {
+			//assert i == 1
+			name := SSinput[i].key
+			if firstdisperseyear > disperseage-ageAtStart {
+				disperseage = firstdisperseyear + ageAtStart
+				fmt.Printf("Warning - Social Security spousal benefit can only be claimed\nafter the spouse claims benefits.\nPlease correct %s's SS age in the configuration file to '%d'.", name, disperseage)
+			} else if disperseage > fraage && firstdisperseyear != disperseage-ageAtStart {
+				if firstdisperseyear <= fraage-ageAtStart {
+					disperseage = fraage
+					fmt.Printf("Warning - Social Security spousal benefits do not increase after FRA,\nresetting benefits start to FRA.\nPlease correct %s's SS age in the configuration file to '%d'.", name, fraage)
 				} else {
-                    //assert i == 1
-                    name = self.SSinput[i]["key"]
-                    if firstdisperseyear > disperseage - ageAtStart {
-                        disperseage = firstdisperseyear + ageAtStart
-                        agestr = "{}-".format(disperseage)
-                        self.SSinput[i]["agestr"] = agestr 
-                        fmt.Printf("Warning - Social Security spousal benefit can only be claimed\nafter the spouse claims benefits.\nPlease correct %s's SS age in the configuration file to '%s'.", name, agestr)
-					} else if disperseage > fraage && firstdisperseyear != disperseage - ageAtStart {
-                        if firstdisperseyear <= fraage - ageAtStart {
-                            disperseage = fraage
-                            agestr = "{}-".format(fraage)
-                            self.SSinput[i]["agestr"] = agestr 
-                            fmt.Printf("Warning - Social Security spousal benefits do not increase after FRA,\nresetting benefits start to FRA.\nPlease correct %s's SS age in the configuration file to '%s'.",name, agestr)
-						} else {
-                            disperseage = firstdisperseyear + ageAtStart
-                            agestr = "{}-".format(disperseage)
-                            self.SSinput[i]["agestr"] = agestr 
-                            fmt.Printf("Warning - Social Security spousal benefits do not increase after FRA,\nresetting benefits start to spouse claim year.\nPlease correct %s's age in the configuration file to '%s'.",name, agestr)
-						}
-					}
-                    fraamount = self.SSinput[0]["amount"]/2 // spousal benefit is 1/2 spouses at FRA 
-                    // alter amount for start age vs fra (minus if before fra)
-                    amount = self.startamount(fraamount, fraage, min(disperseage,fraage))
-				}
-                //print("FRA: %d, FRAamount: %6.0f, Age: %s, amount: %6.0f" % (fraage, fraamount, agestr, amount))
-                self.SSinput[i]["bucket"] = [0] * self.numyr
-                for age := range elist(agestr) {
-                    year = age - ageAtStart //self.startage
-                    if year < 0 {
-                        continue
-					} else if year >= self.numyr {
-                        break
-					} else {
-                        adj_amount = amount * self.i_rate ** (age - currAge) //year
-                        //print("age %d, year %d, bucket: %6.0f += amount %6.0f" %(age, year, bucket[year], adj_amount))
-                        bucket[year] += adj_amount
-                        self.SSinput[i]["bucket"][year] = adj_amount
-					}
+					disperseage = firstdisperseyear + ageAtStart
+					fmt.Printf("Warning - Social Security spousal benefits do not increase after FRA,\nresetting benefits start to spouse claim year.\nPlease correct %s's age in the configuration file to '%d'.", name, disperseage)
 				}
 			}
-            if sections > 1 {
-                //
-                // Must fix up SS for period after one spouse dies
-                //
-                d := make([]float64, 2)
-                d[0] = self.SSinput[0]["throughAge"]-self.SSinput[0]["ageAtStart"]
-                d[1] = self.SSinput[1]["throughAge"]-self.SSinput[1]["ageAtStart"]
-				if d[0] > d[1]  {
-                firstToDie, secondToDie = 1, 0 
-				} else  {
-                firstToDie, secondToDie = 0, 1
-				}
-                for year := range a(d[firstToDie]+1, self.numyr) {
-                    if self.SSinput[0]["bucket"][year] > self.SSinput[1]["bucket"][year] {
-                        greater = self.SSinput[0]["bucket"][year]
-					} else {
-                        greater = self.SSinput[1]["bucket"][year]
-					}
-                    self.SSinput[firstToDie]["bucket"][year] = 0
-                    self.SSinput[secondToDie]["bucket"][year] = greater
-                    bucket[year] = greater
-				}
+			fraamount = SSinput[0].fraamount / 2 // spousal benefit is 1/2 spouses at FRA
+			// alter amount for start age vs fra (minus if before fra)
+			amount = startamount(float64(fraamount), fraage, intMin(disperseage, fraage))
+		}
+		SSinput[i].bucket = make([]float64, ip.numyr) // = [0] * self.numyr
+		SS := make([]float64, ip.numyr)               // = [0] * self.numyr
+		for age := SSinput[i].startAge; age < SSinput[i].endAge; age++ {
+			year := age - ageAtStart //self.startage
+			if year < 0 {
+				// ERROR if ever happens
+				continue
+			} else if year >= ip.numyr {
+				// ERROR if ever happens
+				break
+			} else {
+				adjAmount := amount * math.Pow(iRate, float64(age-currAge)) //year
+				//print("age %d, year %d, SS: %6.0f += amount %6.0f" %(age, year, SS[year], adj_amount))
+				SS[year] += adjAmount
+				SSinput[i].bucket[year] = adjAmount
 			}
+		}
+	}
+	if sections > 1 {
+		//
+		// Must fix up SS for period after one spouse dies
+		//
+		d := make([]int, 2)
+		d[0] = SSinput[0].endAge - SSinput[0].ageAtStart
+		d[1] = SSinput[1].endAge - SSinput[1].ageAtStart
+		firstToDie, secondToDie := 0, 1
+		if d[0] > d[1] {
+			firstToDie, secondToDie = 1, 0
+		}
+		for year := d[firstToDie] + 1; year < ip.numyr; year++ {
+			greater := SSinput[1].bucket[year]
+			if SSinput[0].bucket[year] > SSinput[1].bucket[year] {
+				greater = SSinput[0].bucket[year]
+			}
+			SSinput[firstToDie].bucket[year] = 0
+			SSinput[secondToDie].bucket[year] = greater
+			SS[year] = greater
+		}
+	}
+	SS1 = SSinput[0].bucket
+	SS2 = SSinput[1].bucket
+	if SSinput[0].key != ip.myKey1 {
+		SS1 = SSinput[1].bucket
+		SS2 = SSinput[0].bucket
+	}
+	return SS, SS1, SS2, nil
 }
---
+++ socialsecurity_test.go
@@ -0,0 +1,84 @@
+package rplanlib
+
+import (
+	"testing"
+)
+
+//
+// Testing for socialsecurity.go
+//
+
+func TestFra(t *testing.T) {
+	tests := []struct {
+		age int
+		fra int
+	}{
+		{ // case 0
+			age: 10,
+			fra: 67,
+		},
+		{ // case 1
+			age: 50,
+			fra: 67,
+		},
+		{ // case 1
+			age: 70,
+			fra: 67,
+		},
+	}
+	for i, elem := range tests {
+		fra := fra(elem.age)
+		if elem.fra != fra {
+			t.Errorf("TestFra case %d: expected %d, found %d\n", i, elem.fra, fra)
+		}
+	}
+}
+
+/*
+	tests := []struct {
+	}{
+		{},
+	}
+	for i, elem := range tests {
+	}
+
+			ip: map[string]string{
+				"setName":                    "",
+				"filingStatus":               "",
+				"eT_Age1":            "",
+				"eT_Age2":            "",
+				"eT_RetireAge1":      "",
+				"eT_RetireAge2":      "",
+				"eT_PlanThroughAge1": "",
+				"eT_PlanThroughAge2": "",
+				"eT_PIA1":            "",
+				"eT_PIA2":            "",
+				"eT_SS_Start1":       "",
+				"eT_SS_Start2":       "",
+				"eT_TDRA1":           "",
+				"eT_TDRA2":           "",
+				"eT_TDRA_Rate1":         "",
+				"eT_TDRA_Rate2":         "",
+				"eT_TDRA_Contrib1":   "",
+				"eT_TDRA_Contrib2":   "",
+				"eT_TDRA_ContribStartAge1":   "",
+				"eT_TDRA_ContribStartAge2":   "",
+				"eT_TDRA_ContribEndAge1":     "",
+				"eT_TDRA_ContribEndAge2":     "",
+				"eT_Roth1":                   "",
+				"eT_Roth2":                   "",
+				"eT_Roth_Rate1":              "",
+				"eT_Roth_Rate2":              "",
+				"eT_Roth_Contrib1":           "",
+				"eT_Roth_Contrib2":           "",
+				"eT_Roth_ContribStartAge1":   "",
+				"eT_Roth_ContribStartAge2":   "",
+				"eT_Roth_ContribEndAge1":     "",
+				"eT_Roth_ContribEndAge2":     "",
+				"eT_Aftatax":                 "",
+				"eT_Aftatax_Rate":            "",
+				"eT_Aftatax_Contrib":         "",
+				"eT_Aftatax_ContribStartAge": "",
+				"eT_Aftatax_ContribEndAge":   "",
+			},
+*/

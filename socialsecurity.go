a9cc4cf991c2f68e6c0c2091054d826e0851934e
willauld <will.auld@gmail.com>
2018-02-04 18:58:41 -0800

    adding code and unit tests for social security

--- socialsecurity.go
+++ socialsecurity.go
@@ -4,6 +4,25 @@ import "math"
 import "fmt"
 import "time"
 
+/*
+* Social Security Full Retirement Age (FRA) table
+* Year of Birth *	Full Retirement Age
+*	1937 or earlier		65
+*	1938				65 and 2 months
+*	1939				65 and 4 months
+*	1940				65 and 6 months < 1940 65, 1940 >= 66
+*	1941				65 and 8 months
+*	1942				65 and 10 months
+*	1943--1954			66
+*	1955				66 and 2 months
+*	1956				66 and 4 months
+*	1957				66 and 6 months < 1957 66, 1957 >= 67
+*	1958				66 and 8 months
+*	1959				66 and 10 months
+*	1960 and later		67
+ */
+
+// fra returns the retiree social security fra given current age
 func fra(currAge int) int {
 	thisYear := time.Now().Year()
 	born := thisYear - currAge
@@ -15,18 +34,19 @@ func fra(currAge int) int {
 	return 67
 }
 
-func startamount(amount float64, fra int, start int) float64 {
-	if start > 70 {
-		start = 70
+// adjPIA returns an adjusted PIA amount a retiree would recieve based on their PIA, proposed starting age and fra
+func adjPIA(PIA float64, fra int, startAge int) float64 {
+	if startAge > 70 {
+		startAge = 70
 	}
-	if start < 62 {
-		start = 62
+	if startAge < 62 {
+		startAge = 62
 	}
-	if start < fra {
-		return amount / (math.Pow(1.067, float64(fra-start)))
+	if startAge < fra {
+		return PIA / (math.Pow(1.067, float64(fra-startAge)))
 	}
 	//  start >= fra must be
-	return amount * (math.Pow(1.08, float64(start-fra)))
+	return PIA * (math.Pow(1.08, float64(startAge-fra)))
 }
 
 type SSInput struct {
@@ -47,42 +67,41 @@ func processSS(ip InputParams, r []retiree, iRate float64) (SS, SS1, SS2 []float
 		e := fmt.Errorf("processSS: both PIA1: %d and PIA2: %d are negitive", ip.PIA1, ip.PIA2)
 		return nil, nil, nil, e
 	}
-	SS = make([]float64, ip.numyr)
+	SS = make([]float64, ip.numyr) // = [0] * self.numyr
 
-	sections := 0
 	index := 0
-	sections++
+	sections := 1
 	dt := SSInput{
 		fraamount:  ip.PIA1,            // fraamount := v["amount"]
 		fraage:     fra(ip.age1),       // fraage := v["FRA"]
 		startAge:   ip.SSStart1,        // agestr := v["age"]
 		endAge:     ip.planThroughAge1, // agestr := v["age"]
 		key:        ip.myKey1,
-		ageAtStart: ip.SSStart1,
+		ageAtStart: r[0].ageAtStart,
 		currAge:    ip.age1,
 	}
-	if dt.fraamount < 0 && sections == 1 { // default spousal support in second slot
+	if dt.fraamount < 0 { // place default spousal support in second slot
 		SSinput[1] = dt
 	} else {
 		SSinput[index] = dt
 		index++
 	}
-	sections++
-	dt = SSInput{
-		fraamount:  ip.PIA2,            // fraamount := v["amount"]
-		fraage:     fra(ip.age2),       // fraage := v["FRA"]
-		startAge:   ip.SSStart2,        // agestr := v["age"]
-		endAge:     ip.planThroughAge2, // agestr := v["age"]
-		key:        ip.myKey2,
-		ageAtStart: ip.SSStart2,
-		currAge:    ip.age2,
-	}
-	if dt.fraamount < 0 && sections == 1 { // default spousal support in second slot
-		SSinput[1] = dt
-	} else {
+	if ip.filingStatus == "joint" {
+
+		sections = 2
+		dt = SSInput{
+			fraamount:  ip.PIA2,            // fraamount := v["amount"]
+			fraage:     fra(ip.age2),       // fraage := v["FRA"]
+			startAge:   ip.SSStart2,        // agestr := v["age"]
+			endAge:     ip.planThroughAge2, // agestr := v["age"]
+			key:        ip.myKey2,
+			ageAtStart: r[1].ageAtStart,
+			currAge:    ip.age2,
+		}
 		SSinput[index] = dt
-		index++
 	}
+	//fmt.Printf("SSinput[0]: %v\n", SSinput[0])
+	//fmt.Printf("SSinput[1]: %v\n", SSinput[1])
 	var disperseage int
 	var firstdisperseyear int
 	for i := 0; i < sections; i++ {
@@ -97,35 +116,38 @@ func processSS(ip InputParams, r []retiree, iRate float64) (SS, SS1, SS2 []float
 		var amount float64
 		if fraamount >= 0 {
 			// alter amount for start age vs fra (minus if before fra and + is after)
-			amount = startamount(float64(fraamount), fraage, disperseage)
+			amount = adjPIA(float64(fraamount), fraage, disperseage)
 		} else {
 			//assert i == 1
 			name := SSinput[i].key
 			if firstdisperseyear > disperseage-ageAtStart {
 				disperseage = firstdisperseyear + ageAtStart
-				fmt.Printf("Warning - Social Security spousal benefit can only be claimed\nafter the spouse claims benefits.\nPlease correct %s's SS age in the configuration file to '%d'.", name, disperseage)
+				fmt.Printf("Warning - Social Security spousal benefit can only be claimed\n\tafter the spouse claims benefits.\n\tPlease correct %s's SS age in the configuration file to '%d'.\n", name, disperseage)
 			} else if disperseage > fraage && firstdisperseyear != disperseage-ageAtStart {
 				if firstdisperseyear <= fraage-ageAtStart {
 					disperseage = fraage
-					fmt.Printf("Warning - Social Security spousal benefits do not increase after FRA,\nresetting benefits start to FRA.\nPlease correct %s's SS age in the configuration file to '%d'.", name, fraage)
+					fmt.Printf("Warning - Social Security spousal benefits do not increase after FRA,\n\tresetting benefits start to FRA.\n\tPlease correct %s's SS age in the configuration file to '%d'.\n", name, fraage)
 				} else {
 					disperseage = firstdisperseyear + ageAtStart
-					fmt.Printf("Warning - Social Security spousal benefits do not increase after FRA,\nresetting benefits start to spouse claim year.\nPlease correct %s's age in the configuration file to '%d'.", name, disperseage)
+					fmt.Printf("Warning - Social Security spousal benefits do not increase after FRA,\n\tresetting benefits start to spouse claim year.\n\tPlease correct %s's age in the configuration file to '%d'.\n", name, disperseage)
 				}
 			}
 			fraamount = SSinput[0].fraamount / 2 // spousal benefit is 1/2 spouses at FRA
 			// alter amount for start age vs fra (minus if before fra)
-			amount = startamount(float64(fraamount), fraage, intMin(disperseage, fraage))
+			amount = adjPIA(float64(fraamount), fraage, intMin(disperseage, fraage))
 		}
 		SSinput[i].bucket = make([]float64, ip.numyr) // = [0] * self.numyr
-		SS := make([]float64, ip.numyr)               // = [0] * self.numyr
-		for age := SSinput[i].startAge; age < SSinput[i].endAge; age++ {
-			year := age - ageAtStart //self.startage
+		endage := ip.numyr + SSinput[i].ageAtStart
+		//for age := SSinput[i].startAge; age < endage; age++
+		for age := disperseage; age < endage; age++ {
+			year := age - SSinput[i].ageAtStart //self.startage
 			if year < 0 {
 				// ERROR if ever happens
+				fmt.Printf("ERROR - this should never happen. local code 11111\n")
 				continue
 			} else if year >= ip.numyr {
 				// ERROR if ever happens
+				fmt.Printf("ERROR - this should never happen. local code 22222\n\tage: %d, year: %d, ip.numyr: %d, endPlan: %d, startPlan: %d\n", age, year, ip.numyr, ip.endPlan, ip.startPlan)
 				break
 			} else {
 				adjAmount := amount * math.Pow(iRate, float64(age-currAge)) //year
@@ -156,6 +178,8 @@ func processSS(ip InputParams, r []retiree, iRate float64) (SS, SS1, SS2 []float
 			SS[year] = greater
 		}
 	}
+	//fmt.Printf("SSinput[0]: %v\n", SSinput[0])
+	//fmt.Printf("SSinput[1]: %v\n", SSinput[1])
 	SS1 = SSinput[0].bucket
 	SS2 = SSinput[1].bucket
 	if SSinput[0].key != ip.myKey1 {
--- socialsecurity_test.go
+++ socialsecurity_test.go
@@ -23,7 +23,11 @@ func TestFra(t *testing.T) {
 		},
 		{ // case 1
 			age: 70,
-			fra: 67,
+			fra: 66,
+		},
+		{ // case 1
+			age: 80,
+			fra: 65,
 		},
 	}
 	for i, elem := range tests {
@@ -31,6 +35,244 @@ func TestFra(t *testing.T) {
 		if elem.fra != fra {
 			t.Errorf("TestFra case %d: expected %d, found %d\n", i, elem.fra, fra)
 		}
+	}
+}
+
+func TestAdjPIA(t *testing.T) {
+	tests := []struct {
+		PIA      float64
+		fra      int
+		startAge int
+		adjPIA   float64
+	}{
+		{ // case 0
+			PIA:      10000,
+			fra:      67,
+			startAge: 67,
+			adjPIA:   10000,
+		},
+		{ // case 1
+			PIA:      10000,
+			fra:      66,
+			startAge: 70,
+			adjPIA:   13604,
+		},
+		{ // case 2
+			PIA:      10000,
+			fra:      65,
+			startAge: 62,
+			adjPIA:   8232,
+		},
+	}
+	for i, elem := range tests {
+		amt := adjPIA(elem.PIA, elem.fra, elem.startAge)
+		if int(elem.adjPIA) != int(amt) {
+			t.Errorf("TestAdjPIA case %d: expected %f, found %f\n", i, elem.adjPIA, amt)
+		}
+	}
+}
+
+func TestProcessSS(t *testing.T) {
+	tests := []struct {
+		ip       map[string]string
+		retirees []retiree
+	}{
+		{ // case 0
+			ip: map[string]string{
+				"setName":                    "",
+				"filingStatus":               "joint",
+				"eT_Age1":                    "66",
+				"eT_Age2":                    "64",
+				"eT_RetireAge1":              "66",
+				"eT_RetireAge2":              "66",
+				"eT_PlanThroughAge1":         "76",
+				"eT_PlanThroughAge2":         "76",
+				"eT_PIA1":                    "20", //20k
+				"eT_PIA2":                    "-1", // spousal benefit
+				"eT_SS_Start1":               "66",
+				"eT_SS_Start2":               "66",
+				"eT_TDRA1":                   "",
+				"eT_TDRA2":                   "",
+				"eT_TDRA_Rate1":              "",
+				"eT_TDRA_Rate2":              "",
+				"eT_TDRA_Contrib1":           "",
+				"eT_TDRA_Contrib2":           "",
+				"eT_TDRA_ContribStartAge1":   "",
+				"eT_TDRA_ContribStartAge2":   "",
+				"eT_TDRA_ContribEndAge1":     "",
+				"eT_TDRA_ContribEndAge2":     "",
+				"eT_Roth1":                   "",
+				"eT_Roth2":                   "",
+				"eT_Roth_Rate1":              "",
+				"eT_Roth_Rate2":              "",
+				"eT_Roth_Contrib1":           "",
+				"eT_Roth_Contrib2":           "",
+				"eT_Roth_ContribStartAge1":   "",
+				"eT_Roth_ContribStartAge2":   "",
+				"eT_Roth_ContribEndAge1":     "",
+				"eT_Roth_ContribEndAge2":     "",
+				"eT_Aftatax":                 "",
+				"eT_Aftatax_Rate":            "",
+				"eT_Aftatax_Contrib":         "",
+				"eT_Aftatax_ContribStartAge": "",
+				"eT_Aftatax_ContribEndAge":   "",
+			},
+			retirees: []retiree{
+				{ // retireeindx == 0
+					age:        66,
+					ageAtStart: 66,
+					throughAge: 76,
+					mykey:      "retiree1",
+					definedContributionPlan: false,
+					dcpBuckets:              nil,
+				},
+				{ // retireeindx == 1
+					age:        64,
+					ageAtStart: 64,
+					throughAge: 76,
+					mykey:      "retiree2",
+					definedContributionPlan: false,
+					dcpBuckets:              nil,
+				},
+			},
+		},
+		{ // case 1
+			ip: map[string]string{
+				"setName":                    "",
+				"filingStatus":               "joint",
+				"eT_Age1":                    "66",
+				"eT_Age2":                    "64",
+				"eT_RetireAge1":              "66",
+				"eT_RetireAge2":              "66",
+				"eT_PlanThroughAge1":         "76",
+				"eT_PlanThroughAge2":         "76",
+				"eT_PIA1":                    "-1", //Spousal ben
+				"eT_PIA2":                    "20", //20k
+				"eT_SS_Start1":               "66",
+				"eT_SS_Start2":               "70",
+				"eT_TDRA1":                   "",
+				"eT_TDRA2":                   "",
+				"eT_TDRA_Rate1":              "",
+				"eT_TDRA_Rate2":              "",
+				"eT_TDRA_Contrib1":           "",
+				"eT_TDRA_Contrib2":           "",
+				"eT_TDRA_ContribStartAge1":   "",
+				"eT_TDRA_ContribStartAge2":   "",
+				"eT_TDRA_ContribEndAge1":     "",
+				"eT_TDRA_ContribEndAge2":     "",
+				"eT_Roth1":                   "",
+				"eT_Roth2":                   "",
+				"eT_Roth_Rate1":              "",
+				"eT_Roth_Rate2":              "",
+				"eT_Roth_Contrib1":           "",
+				"eT_Roth_Contrib2":           "",
+				"eT_Roth_ContribStartAge1":   "",
+				"eT_Roth_ContribStartAge2":   "",
+				"eT_Roth_ContribEndAge1":     "",
+				"eT_Roth_ContribEndAge2":     "",
+				"eT_Aftatax":                 "",
+				"eT_Aftatax_Rate":            "",
+				"eT_Aftatax_Contrib":         "",
+				"eT_Aftatax_ContribStartAge": "",
+				"eT_Aftatax_ContribEndAge":   "",
+			},
+			retirees: []retiree{
+				{ // retireeindx == 0
+					age:        66,
+					ageAtStart: 66,
+					throughAge: 76,
+					mykey:      "retiree1",
+					definedContributionPlan: false,
+					dcpBuckets:              nil,
+				},
+				{ // retireeindx == 1
+					age:        64,
+					ageAtStart: 64,
+					throughAge: 76,
+					mykey:      "retiree2",
+					definedContributionPlan: false,
+					dcpBuckets:              nil,
+				},
+			},
+		},
+	}
+	for i, elem := range tests {
+		ip := NewInputParams(elem.ip)
+		iRate := 1.03
+		ss, ss1, ss2, err := processSS(ip, elem.retirees, iRate)
+		if err != nil {
+			t.Errorf("TestProcessSS case %d: %s\n", i, err)
+		}
+		if ip.filingStatus != "joint" {
+			if len(ss) != len(ss1) {
+				t.Errorf("TestProcessSS case %d: Social Security vectors are not the same lens as required\n", i)
+			}
+			for j := 0; j < len(ss); j++ {
+				if ss[j] != ss1[j] {
+					t.Errorf("TestProcessSS case %d:  SS[j] must equal SS1[j]\n", i)
+
+				}
+			}
+		} else {
+			if len(ss) != len(ss1) || len(ss) != len(ss2) {
+				t.Errorf("TestProcessSS case %d: Social Security vectors are not the same lens as required\n", i)
+			}
+			for j := 0; j < len(ss); j++ {
+				if ss[j] != ss1[j]+ss2[j] {
+					t.Errorf("TestProcessSS case %d:  SS[j] must equal SS1[j] + SS2[j]\n", i)
+
+				}
+			}
+		}
+		zeros := ip.SSStart1 - ip.startPlan
+		// Verify years before starting SS have zero SS income
+		for j := 0; j < zeros; j++ {
+			if ss1[j] != 0 {
+				t.Errorf("TestProcessSS case %d:  ss1[%d]: %f should equal zero, it's before starting SS\n", i, j, ss1[j])
+			}
+		}
+		// varify that years after retiree's planthrough have zero SS income
+		r1end := ip.planThroughAge1 - elem.retirees[0].ageAtStart + 1
+		for j := r1end; j < ip.numyr; j++ {
+			if ss1[j] != 0 {
+				t.Errorf("TestProcessSS case %d:  ss1[%d]: %f should equal zero, it's after planThrough age\n", i, j, ss1[j])
+			}
+		}
+
+		delta := ip.age2 - ip.age1
+		zeros = ip.SSStart2 - delta - ip.startPlan // convert to prime age
+		// Verify years before starting SS have zero SS income
+		for j := 0; j < zeros; j++ {
+			if int(ss2[j]) != 0 {
+				t.Errorf("TestProcessSS case %d:  ss2[%d]: %f should equal zero, it's before starting SS\n", i, j, ss2[j])
+			}
+		}
+		// varify that years after retiree's planthrough have zero SS income
+		r2end := ip.planThroughAge2 - elem.retirees[1].ageAtStart + 1
+		for j := r2end; j < ip.numyr; j++ {
+			if ss2[j] != 0 {
+				t.Errorf("TestProcessSS case %d:  ss2[%d]: %f should equal zero, it's after planThrough age\n", i, j, ss2[j])
+			}
+		}
+		if r1end < r2end {
+			// Verify retiree2 gets greater SS after retiree1 is gone
+			woulda := iRate * ss1[r1end-1]
+			if ss2[r1end] < woulda {
+				t.Errorf("TestProcessSS case %d:  ss2[%d]: %f should have gotten %f after spouses death\n", i, r1end, ss2[r1end], woulda)
+			}
+		} else { // equal case does not muck up this test
+			// Verify retiree1 gets greater SS after retiree2 is gone
+			woulda := iRate * ss2[r2end-1]
+			if ss1[r1end] < woulda {
+				t.Errorf("TestProcessSS case %d:  ss1[%d]: %f should have gotten %f after spouses death\n", i, r2end, ss2[r2end], woulda)
+			}
+
+		}
+
+		//fmt.Printf("len ss: %d\n", len(ss))
+		//fmt.Printf(" ss: %v\n", ss)
+		//fmt.Printf("ss1: %v\n", ss1)
+		//fmt.Printf("ss2: %v\n", ss2)
 	}
 }
 

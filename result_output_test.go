5d47633496264b95e2713e86168e8af0e9355f4d
willauld <will.auld@gmail.com>
2018-02-07 17:34:13 -0800

    extending unit test

--- result_output_test.go
+++ result_output_test.go
@@ -20,22 +20,22 @@ func TestResultsOutput(t *testing.T) {
 		allowDeposits bool
 		iRate         float64
 	}{
-		{ // Case 0 // joint
+		{ // Case 0 // case to match mobile.toml
 			ip: map[string]string{
 				"setName":                    "activeParams",
 				"filingStatus":               "joint",
-				"eT_Age1":                    "65",
-				"eT_Age2":                    "63",
-				"eT_RetireAge1":              "66",
-				"eT_RetireAge2":              "66",
-				"eT_PlanThroughAge1":         "100",
-				"eT_PlanThroughAge2":         "100",
-				"eT_PIA1":                    "30", // 30k
-				"eT_PIA2":                    "-1",
-				"eT_SS_Start1":               "70",
-				"eT_SS_Start2":               "66",
+				"eT_Age1":                    "54",
+				"eT_Age2":                    "54",
+				"eT_RetireAge1":              "65",
+				"eT_RetireAge2":              "65",
+				"eT_PlanThroughAge1":         "75",
+				"eT_PlanThroughAge2":         "75",
+				"eT_PIA1":                    "",
+				"eT_PIA2":                    "",
+				"eT_SS_Start1":               "",
+				"eT_SS_Start2":               "",
 				"eT_TDRA1":                   "200", // 200k
-				"eT_TDRA2":                   "100", // 100k
+				"eT_TDRA2":                   "",
 				"eT_TDRA_Rate1":              "",
 				"eT_TDRA_Rate2":              "",
 				"eT_TDRA_Contrib1":           "",
@@ -45,7 +45,7 @@ func TestResultsOutput(t *testing.T) {
 				"eT_TDRA_ContribEndAge1":     "",
 				"eT_TDRA_ContribEndAge2":     "",
 				"eT_Roth1":                   "",
-				"eT_Roth2":                   "50", // 50k
+				"eT_Roth2":                   "",
 				"eT_Roth_Rate1":              "",
 				"eT_Roth_Rate2":              "",
 				"eT_Roth_Contrib1":           "",
@@ -54,11 +54,15 @@ func TestResultsOutput(t *testing.T) {
 				"eT_Roth_ContribStartAge2":   "",
 				"eT_Roth_ContribEndAge1":     "",
 				"eT_Roth_ContribEndAge2":     "",
-				"eT_Aftatax":                 "50", // 50k
-				"eT_Aftatax_Rate":            "7.25",
+				"eT_Aftatax":                 "",
+				"eT_Aftatax_Rate":            "",
 				"eT_Aftatax_Contrib":         "",
 				"eT_Aftatax_ContribStartAge": "",
 				"eT_Aftatax_ContribEndAge":   "",
+
+				"eT_iRate":    "2.5",
+				"eT_rRate":    "6",
+				"eT_maximize": "Spending", // or "PlusEstate"
 			},
 			verbose:       true,
 			allowDeposits: false,
@@ -66,8 +70,9 @@ func TestResultsOutput(t *testing.T) {
 		},
 	}
 	for i, elem := range tests {
-		ti := NewTaxInfo(elem.ip["filingStatus"])
 		ip := NewInputParams(elem.ip)
+		fmt.Printf("InputParams: %#v\n", ip)
+		ti := NewTaxInfo(ip.filingStatus)
 		taxbins := len(*ti.Taxtable)
 		cgbins := len(*ti.Capgainstable)
 		vindx, err := NewVectorVarIndex(ip.numyr, taxbins,
@@ -111,5 +116,18 @@ func TestResultsOutput(t *testing.T) {
 		fmt.Printf(str)
 		fmt.Printf("Calling LPSimplex() for m:%d x n:%d model\n", len(a), len(a[0]))
 
+		/*
+			ms.print_model_results(res.x)
+				        if args.verboseincome:
+				            print_income_expense_details()
+				        if args.verboseaccounttrans:
+				            print_account_trans(res)
+				        if args.verbosetax:
+				            print_tax(res)
+				        if args.verbosetaxbrackets:
+				            print_tax_brackets(res)
+							print_cap_gains_brackets(res)
+		*/
+		ms.printBaseConfig(&res.X)
 	}
 }

00bc5d5d3b305f3f210c9e7a756b615f74dd8393
willauld <will.auld@gmail.com>
2018-05-14 18:54:53 -0700

    Adding version release script

---
+++ createLibRelease.ps1
@@ -0,0 +1,73 @@
+param(
+    [string]$vermajor = "0", 
+    [string]$verminor = "4",
+    [string]$verStr = "alpha",
+    [switch]$resetPatch = $false,
+    [switch]$tagRepo = $false
+)
+
+# file has current patch number mananged by this script
+$verPatchFile = ".patchNum" 
+
+#Test-Path $verPatchFile -PathType Leaf
+if ($resetPatch -or !(Test-Path $verPatchFile -PathType Leaf)) {
+    $zero = 0
+    Set-Content $verPatchFile -value $zero 
+}
+$verpatch = Get-Content .patchNum
+[int]$verpatchInt = $verpatch
+Write-Output "verpatch is: $verpatch, Int $verpatchInt"
+
+$newPatchNum = 1 + $verpatchInt
+[string]$newPatchStr = $newPatchNum
+
+Set-Content $verPatchFile -value $newPatchStr
+$verpatch = Get-Content .patchNum
+[int]$verpatchInt = $verpatch
+Write-Output "verpatch is: $verpatch, Int $verpatchInt"
+
+$rplanlibversionFile = "
+    package rplanlib
+
+var (
+    // See createLibRelease.ps1 for variable definition / values
+    vermajor      string
+    verminor      string
+    verpatch      string
+    verstr        string
+)
+
+var version = struct {
+    major         string
+    minor         string
+    patch         string
+    str           string
+} {""$vermajor"", 
+    ""$verminor"", ""$verpatch"",
+    ""$verstr"" }
+"
+#Write-Output "version file will be: $rplanlibversionFile"
+
+$rplanlibversionFileName = "rplanlibversion.go"
+
+
+if ($tagRepo) {
+    Write-Output "Tagging the repo"
+
+    $tagStr = "v$vermajor.$verminor.$verpatch"
+
+    Set-Content $rplanlibversionFileName -value $rplanlibversionFile
+    git commit -m "Updating version file for $tagStr" $rplanlibversionFileName 
+
+    git tag -a $tagStr -m "Tag version $tagStr"
+    git push origin $tagStr
+
+    # to delete tags if needed:
+    #git tag --delete $tagStr
+    #git push --delete origin $tagStr
+}
+
+$libgitver = git describe --always
+
+Write-Output "rplanlib git hash: $libgitver"
+
--- versionBuild.ps1
+++
@@ -1,6 +0,0 @@
-#go build -ldflags "-X main.buildstamp `date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash `git rev-parse HEAD`" myapp.go
-Set-PSDebug -Trace 1
-#Set-PSDebug -Trace 2
-go build -ldflags "-X rplanlib.version.BuildTime `date -u '+%Y-%m-%d_%I:%M:%S%p'` -X rplanlib.version.Version 1.5"
-
-Set-PSDebug -Trace 0 
